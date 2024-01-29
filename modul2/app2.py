c680c693e0a2a91d27d1ea035498056323bbadfb
Andrei_Cirtina <andrei_cirtina@yahoo.com>
2024-01-29 21:04:18 +0200

    new files

---
+++ modul2/app2.py
@@ -0,0 +1,13 @@
+
+h = input('input height:')
+b = input ('input base:')
+h = int(h)
+b = int (b)
+print(h + b)
+
+A = (1/2) * b * h
+print(A)
+
+#prn = print('out')
+#print(prn)
+#print(type(prn))
---
+++ modul2/app3.py
@@ -0,0 +1,4 @@
+password = 7710
+user_pass = (int(input('Enter password: ')))
+print(user_pass)
+print(password == user_pass )
